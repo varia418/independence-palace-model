4f368d54c55b88de0efafcedd2a71feb89b658b7
varia <ltnhan2610@gmail.com>
2023-12-06 00:33:36 +0700

    init sequelize

---
+++ db/index.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('sqlite::memory:');
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
