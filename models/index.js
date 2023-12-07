d73d61fc33da748f031fcc60cada146f95927dc8
varia <ltnhan2610@gmail.com>
2023-12-07 17:03:08 +0700

    sync models with db

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const dbConfig = require("../config/db.config.js");
+const fs = require('fs');
+const path = require('path');
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
@@ -19,6 +21,20 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.tutorials = require("./box.model.js")(sequelize, Sequelize);
+const basename = path.basename(__filename);
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js' &&
+            file.indexOf('.test.js') === -1
+        );
+    })
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize);
+        db[model.name] = model;
+    });
 
 module.exports = db;
