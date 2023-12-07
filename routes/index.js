a6b3d6b20a1f8f207b79b7e783f785bba3b08f98
varia <ltnhan2610@gmail.com>
2023-12-07 19:54:57 +0700

    handle error route

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,8 @@ router.get('/login', function (req, res, next) {
     res.render('login', { title: 'login' });
 });
 
+router.get('/*', function (req, res, next) {
+    res.render('error', { title: 'error' });
+});
+
 module.exports = router;
