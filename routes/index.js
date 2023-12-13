cb56d41a7defb353fb213f2d53fe2433099e21b8
varia <ltnhan2610@gmail.com>
2023-12-13 11:10:13 +0700

    rename geometryController to buildingController and update env

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var geometryController = require('../controllers/geometry.controller');
+var buildingController = require('../controllers/building.controller');
 
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'index' });
@@ -14,7 +14,7 @@ router.get('/contact', function (req, res, next) {
     res.render('contact', { title: 'contact' });
 });
 
-router.get('/map', geometryController.renderDDL);
+router.get('/map', buildingController.renderDDL);
 
 router.get('/login', function (req, res, next) {
     res.render('login', { title: 'login' });
