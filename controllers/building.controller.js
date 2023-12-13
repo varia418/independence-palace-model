f427b80e780e82c09254a3cb835b6af853c64155
varia <ltnhan2610@gmail.com>
2023-12-13 11:33:26 +0700

    query component parts

--- controllers/building.controller.js
+++ controllers/building.controller.js
@@ -1,6 +1,9 @@
 const db=require("../models");
 const BuildingComponent=db.buildingComponent;
 const Box=db.box;
+const Cylinder=db.cylinder;
+const Sphere=db.sphere;
+const Plane=db.plane;
 const Location=db.location;
 const Size=db.size;
 const Rotate=db.rotate;
@@ -9,7 +12,21 @@ const Offset=db.offset;
 const renderDDL=async (req, res) => {
     try {
         const components=await BuildingComponent.findAll({ include: { all: true } });
-        res.render('map', { data: JSON.stringify(components) });
+
+        const data=await Promise.all(components.map(async component => {
+            const boxes=await Box.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
+            component.dataValues.boxes=boxes;
+            const cylinders=await Cylinder.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
+            component.dataValues.cylinders=cylinders;
+            const spheres=await Sphere.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
+            component.dataValues.spheres=spheres;
+            const planes=await Plane.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
+            component.dataValues.planes=planes;
+
+            return component;
+        }));
+
+        res.render('map', { data: JSON.stringify(data) });
     } catch (error) {
         res.status(500).send({
             message:
