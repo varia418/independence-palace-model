6f01b9a366a775d1f333eceb607e58c437d103b1
varia <ltnhan2610@gmail.com>
2023-12-19 12:12:34 +0700

    render pyramids

--- controllers/building.controller.js
+++ controllers/building.controller.js
@@ -1,6 +1,7 @@
 const db=require("../models");
 const BuildingComponent=db.buildingComponent;
 const Box=db.box;
+const Pyramid=db.pyramid;
 const Cylinder=db.cylinder;
 const Sphere=db.sphere;
 const Plane=db.plane;
@@ -16,6 +17,8 @@ const renderDDL=async (req, res) => {
         const data=await Promise.all(components.map(async component => {
             const boxes=await Box.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
             component.dataValues.boxes=boxes;
+            const pyramids=await Pyramid.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
+            component.dataValues.pyramids=pyramids;
             const cylinders=await Cylinder.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
             component.dataValues.cylinders=cylinders;
             const spheres=await Sphere.findAll({ where: { buildingComponentId: component.id }, include: [{ model: Location }, { model: Size }, { model: Rotate }, { model: Offset }] });
